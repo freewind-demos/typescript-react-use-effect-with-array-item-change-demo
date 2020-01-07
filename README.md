cd9d336a0634c626a6a34f78c7d8332ebb77e737
Freewind <nowindlee@gmail.com>
2020-01-07 23:57:03 +0800

    typescript-react-use-effect-with-array-item-change-demo

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React "useEffect" with Array Item Change Demo
+========================================================
+
+对于`useEffect`，如果直接修改dependency的属性，不会trigger useEffect。
+必须生成一个新的dependency才行，可以手动生成，或者用clone的方式。
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,23 @@
-//import * as React from 'react'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import cloneDeep from 'lodash/cloneDeep';
 
 export default function Hello() {
+
+  const [data, setData] = useState([
+    {name: 'typescript'}
+  ])
+
+  useEffect(() => {
+    console.log('### data is changed', data);
+  }, [data])
+
   return <div>
     <h1>Hello React</h1>
+    <input type='text' defaultValue={data[0].name} onChange={event => {
+      const value = event.target.value;
+      // FIXME change object directly doesn't trigger useEffect
+      data[0].name = value
+      setData(cloneDeep(data));
+    }}/>
   </div>
 };
